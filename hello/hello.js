6fda5c37af8dac01499cbc77ce83dc0f50fcee90
unknown <stefan.ionica2000@gmail.com>
2022-01-18 21:04:05 +0200

    JS_CORE

---
+++ hello/hello.js
@@ -0,0 +1,17 @@
+// ruleaza tellMe dupa 3000 ms (3 sec):
+setTimeout(tellMe, 3000)
+
+// creaza o noua instructiune (functie) numita tellMe
+function tellMe() {
+    alert("Hello world!")
+    alert("Welcome to JavaScript!")
+}
+
+
+// comment (ctrl+/) pentru o singura linie
+
+/*comment pentru un text mare 
+; pentru mai multe linii
+*/ 
+
+
---
+++ hello/variabile/variabile.js
@@ -0,0 +1,14 @@
+// punem data curenta in variabila date
+var date = new Date()
+
+var hour = date.getHours()
+var minutes = date.getMinutes()
+var seconds = date.getSeconds()
+var isRaining = false
+// am adaugat la un sir o variabila (concatenare)
+var message = "Hello! Este ora " + hour + ":" + minutes + ":" + seconds
+// o variabila fara valoare (undefined)
+var city
+
+
+alert(message)
