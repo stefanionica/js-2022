af34d5f1d855e3b160f963c39d27aa2cc73cfbdb
unknown <stefan.ionica2000@gmail.com>
2022-01-20 19:10:49 +0200

    js_core

---
+++ js-2022/decizii/if.js
@@ -0,0 +1,40 @@
+
+var isRaining = false
+var isSunny = false
+var isSnowing = true
+
+//if (isRaining) document.write('Nu uita de umbrela!')
+//daca este o sing instructiune
+
+
+//if cu mai multe ramuri
+if (isRaining) {
+    document.write('Nu uita de umbrela!')
+    document.write('Si  nu uita de pelerina!')
+} else if(isSunny) {
+    document.write('Mancam o pizza la terasa')
+}  else if(isSnowing) {
+    document.write('Facem un om de zapada')
+} else {
+    document.write('Stai acasa')
+}
+
+// if simplu
+if (condition) {
+    
+}
+// if cu else
+if (condition) {
+    
+} else {
+    
+}
+
+
+
+
+
+
+
+
+
