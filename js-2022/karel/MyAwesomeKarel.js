c8951c84a73169fc6482511eff99a7c835e24afd
unknown <stefan.ionica2000@gmail.com>
2022-02-01 17:31:11 +0200

    js_core

---
+++ js-2022/karel/MyAwesomeKarel.js
@@ -0,0 +1,156 @@
+
+this.turnRight = function() {
+this.turnLeft()
+this.turnLeft()
+this.turnLeft()
+}
+
+
+this.makeSteps = function(steps) {
+for(var i = 0; i < steps; i++) {
+this.move() 
+}
+}
+
+var beepersPresent = false
+var frontIsBlocked = true
+
+
+
+this.turnLeft()
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.turnRight()
+}
+
+this.makeSteps(4)
+this.turnRight()
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.turnLeft()
+}
+this.makeSteps(4)
+this.turnLeft()
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+this.turnRight()
+this.makeSteps(4)
+this.turnRight()
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (true) {
+this.putBeeper() 
+this.move()
+} else {
+this.move()
+}
+if (false) {
+this.putBeeper() 
+} 
+
+if (true) {
+this.getLocation()
+}
+
